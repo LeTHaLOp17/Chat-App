d283f459894b701e8f11387e06f03241fdbaf97b
Ayush Kumar Patel <110476026+LeTHaLOp17@users.noreply.github.com>
2024-05-12 23:47:49 +0530

    Add files via upload

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const generateTokenAndSetCookie = (userId, res) => {
+	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "12d",
+	});
+
+	res.cookie("jwt", token, {
+		maxAge: 12 * 24 * 60 * 60 * 1000, // MS
+		httpOnly: true, // prevent attacks
+		sameSite: "strict", // CSRF attacks 
+		secure: process.env.NODE_ENV !== "development",
+	});
+};
+
+export default generateTokenAndSetCookie;
